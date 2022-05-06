6ec36c54b14b26303ef1f98fa2816486b57f74e7
ecolorfz <minghaozheng53@gmail.com>
2022-05-06 23:54:16 +0800

    helloworldexample

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from scene import Scene
 import taichi as ti
 from taichi.math import *
 
-scene = Scene(exposure=10)
-scene.set_floor(-0.05, (1.0, 1.0, 1.0))
-scene.set_background_color((1.0, 0, 0))
+scene = Scene(voxel_edges=0, exposure=2) # 创建场景，指定体素描边宽度和曝光值
+scene.set_floor(0, (1.0, 1.0, 1.0)) # 地面高度
+scene.set_background_color((0.5, 0.5, 0.4)) # 天空颜色
+scene.set_directional_light((1, 1, -1), 0.2, (1, 0.8, 0.6)) # 光线方向和颜色
 
 
 @ti.kernel
 def initialize_voxels():
-    # Your code here! :-)
-    scene.set_voxel(vec3(0, 0, 0), 2, vec3(0.9, 0.1, 0.1))
-
+    scene.set_voxel(vec3(0), 1, vec3(1)) # 在 (0, 0, 0) 加入一个白色 (1, 1, 1) 的体素！
 
 initialize_voxels()
 
